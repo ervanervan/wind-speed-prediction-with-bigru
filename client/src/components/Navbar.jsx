bfde411cc8b6f6bc3b72691680f478ace4e3bebe
ervanervan <ervanervan040@gmail.com>
2024-05-15 19:58:59 +0700

    update: responsive homepage

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.svg";
+import logoWhite from "../assets/logo-white.svg";
+import IconMenu from "../assets/icons/IconMenu";
+import IconClose from "../assets/icons/IconClose";
+import gradientLCL from "../assets/gradient-l-c-l.svg";
+import gradientFBR from "../assets/gradient-f-b-r.svg";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
   return (
     <header className="container mx-auto py-6 px-5">
       <nav className="flex items-center justify-between py-2">
@@ -30,7 +41,61 @@ export default function Navbar() {
             </Link>
           </div>
         </div>
-        <div className="text-white-1 md:hidden">Menu</div>
+        <button className="md:hidden" onClick={handleToggle}>
+          <IconMenu className="size-8 stroke-white-2" />
+        </button>
+
+        <div
+          className={`fixed h-screen w-full top-0 right-0 z-50 bg-dark text-textBlack flex flex-col items-start justify-start shadow-lg gap-y-4 px-5 pt-[7.5rem] pb-8 transition-all duration-500 transform ${
+            isOpen
+              ? "translate-x-[0%] ease-in-out"
+              : "-translate-x-[100%] ease-in-out"
+          }`}
+        >
+          <div className="text-textBlack flex flex-col items-start justify-center w-full gap-y-5 sm:px-14">
+            <Link
+              to={"/anambas"}
+              className="py-3 text-white-1 font-medium text-xl"
+            >
+              Anambas
+            </Link>
+            <hr className="border[.5px] border-bgBlack2 w-full" />
+
+            <Link
+              to={"/tanjungpinang"}
+              className="py-3 text-white-1 font-medium text-xl"
+            >
+              Tanjungpinang
+            </Link>
+            <hr className="border[.5px] border-bgBlack2 w-full" />
+          </div>
+
+          {/* <img
+            src={gradientLTR}
+            alt=""
+            className="absolute -top-48 right-0 -z-10"
+          /> */}
+          <img
+            src={gradientLCL}
+            alt=""
+            className="absolute -top-96 left-0 -z-10"
+          />
+          <img
+            src={gradientFBR}
+            alt=""
+            className="absolute bottom-0 right-0 -z-10"
+          />
+
+          <Link to={"/"} className="absolute top-8 left-5 sm:left-16">
+            <img src={logoWhite} alt="" className="w-32" />
+          </Link>
+          <button
+            className="absolute top-8 right-5 sm:right-16"
+            onClick={handleToggle}
+          >
+            <IconClose className="size-8 stroke-white-2" />
+          </button>
+        </div>
       </nav>
     </header>
   );
