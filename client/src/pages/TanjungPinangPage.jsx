94d50c6f49d6895766c63f4b60cc1d30b9431b84
ervanervan <ervanervan040@gmail.com>
2024-05-16 11:19:45 +0700

    add & update: tanjungpinangpage

--- client/src/pages/TanjungPinangPage.jsx
+++ client/src/pages/TanjungPinangPage.jsx
@@ -5,40 +5,63 @@ import gradientTL from "../assets/gradient-t-l.svg";
 import gradientBL from "../assets/gradient-b-l.svg";
 import gradientTR from "../assets/gradient-t-r.svg";
 import gradientBR from "../assets/gradient-b-r.svg";
+import FF_AVG_ANB from "./FF_AVG_ANB";
+import FF_X_ANB from "./FF_X_ANB";
+import IconAir from "../assets/icons/IconAir";
+import Tabs from "../components/Tabs";
+import Footer from "../components/Footer";
 
 const TanjungPinangPage = () => {
-  return (
-    <section className="relative">
-      <Navbar />
-      <img src={gradientTL} alt="" className="absolute top-12 left-0 -z-10" />
-      <img src={gradientTR} alt="" className="absolute top-0 right-0 -z-10" />
-      <img
-        src={gradientBL}
-        alt=""
-        className="absolute -bottom-40 left-0 -z-10"
-      />
-      <img
-        src={gradientBR}
-        alt=""
-        className="absolute bottom-0 right-0 -z-10"
-      />
+  const tabs = [
+    {
+      label: "Ff_Avg",
+      icon: IconAir,
+      content: <FF_AVG_ANB />,
+    },
+    {
+      label: "Ff_X",
+      icon: IconAir,
+      content: <FF_X_ANB />,
+    },
+  ];
 
-      <div className="container mx-auto relative">
-        <div>
-          <div className="md:mt-12 px-5 py-20 relative">
-            <img
-              src={PatternHero}
-              alt=""
-              className="absolute top-1/2 -translate-y-1/2 left-1/2 transform -translate-x-1/2 -z-10 hidden lg:block"
-            />
-            <h1 className="text-white-2 font-medium text-4xl lg:text-5xl flex items-center justify-center text-center leading-[3.5rem] md:leading-[4rem]">
-              Prediksi Kecepatan Angin <br />
-              Rata-Rata Kota Tanjungpinang
-            </h1>
+  return (
+    <>
+      <section className="relative">
+        <Navbar />
+        <img src={gradientTL} alt="" className="absolute top-12 left-0 -z-10" />
+        <img src={gradientTR} alt="" className="absolute top-0 right-0 -z-10" />
+        <img
+          src={gradientBL}
+          alt=""
+          className="absolute -bottom-40 left-0 -z-10"
+        />
+        <img
+          src={gradientBR}
+          alt=""
+          className="absolute bottom-0 right-0 -z-10"
+        />
+
+        <div className="container mx-auto relative">
+          <div>
+            <div className="md:my-12 px-5 py-20 relative">
+              <img
+                src={PatternHero}
+                alt=""
+                className="absolute top-1/2 -translate-y-1/2 left-1/2 transform -translate-x-1/2 -z-10 hidden lg:block"
+              />
+              <h1 className="text-white-2 font-medium text-4xl lg:text-5xl flex items-center justify-center text-center leading-[3.5rem] md:leading-[4rem]">
+                Prediksi Kecepatan Angin <br />
+                Kota Tanjungpinang
+              </h1>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+
+      <Tabs tabs={tabs} />
+      <Footer />
+    </>
   );
 };
 
