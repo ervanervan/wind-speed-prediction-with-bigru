fab6c1adc2236624efda1960c62a631c9e9cced3
ervanervan <ervanervan040@gmail.com>
2024-07-10 14:22:43 +0700

    update: ploting rmse mape akurasi ffxtpi

--- server/ploting_rmse.py
+++ server/ploting_rmse.py
@@ -35,12 +35,17 @@ import matplotlib.pyplot as plt
 # # plt.savefig("RMSE_Train"+model_name+".jpeg", format='jpeg', dpi=1000)
 # plt.show()
 
+
+
 # RMSE TRAINING
-rmse_train = [0.6474, 0.6443, 0.6423, 0.6413, 0.6441, 0.6508, 0.6455, 0.6463, 
-              0.6448, 0.6447, 0.6471, 0.6464, 0.6575, 0.6479, 0.6522, 0.6530, 
-              0.6455, 0.6464, 0.6471, 0.6472]
+# rmse_train = [0.6474, 0.6443, 0.6423, 0.6413, 0.6441, 0.6508, 0.6455, 0.6463, 
+#               0.6448, 0.6447, 0.6471, 0.6464, 0.6575, 0.6479, 0.6522, 0.6530, 
+#               0.6455, 0.6464, 0.6471, 0.6472]
+rmse_train = [1.7415, 1.7395, 1.7131, 1.7087, 1.7324, 1.7461, 1.7391, 1.7321, 
+              1.7208, 1.7176, 1.7456, 1.7435, 1.7658, 1.7658, 1.7421, 1.8079, 
+              1.7431, 1.7470, 1.7401, 1.7374]
 data_rmse_train = [i+1 for i in range(len(rmse_train))]
-model_name = "FFAVGTPI"
+model_name = "FFXTPI"
 
 # Plot RMSE Training dengan garis tebal dan grid horizontal
 plt.figure(figsize=(14, 7))
@@ -55,15 +60,18 @@ plt.show()
 
 
 # MAPE TRAINING
-mape_train = [31.35, 31.14, 31.00, 31.14, 32.16, 32.66, 31.05, 31.01, 31.14, 31.34, 
-              31.39, 31.05, 29.95, 30.97, 32.79, 31.31, 31.30, 31.02, 30.83, 31.64]
+# mape_train = [31.35, 31.14, 31.00, 31.14, 32.16, 32.66, 31.05, 31.01, 31.14, 31.34, 
+#               31.39, 31.05, 29.95, 30.97, 32.79, 31.31, 31.30, 31.02, 30.83, 31.64]
+mape_train = [22.75, 22.21, 22.81, 22.78, 21.86, 23.12, 22.99, 22.27, 23.81, 
+              22.94, 22.86, 23.54, 21.89, 21.74, 22.35, 24.95, 23.67, 22.60, 
+              23.14, 22.96]
 data_mape_train = [i+1 for i in range(len(mape_train))]
 
 # Plot MAPE Training dengan garis tebal dan grid horizontal
 plt.figure(figsize=(14, 7))
 plt.plot(data_mape_train, mape_train, linewidth=2)  # Set garis lebih tebal
 plt.title('MAPE Data Training')
-plt.ylabel('MAPE')
+plt.ylabel('MAPE (%)')
 plt.xlabel('Percobaan')
 plt.legend(['MAPE'], loc='upper right')
 plt.grid(True)  # Tampilkan hanya grid horizontal
@@ -71,3 +79,21 @@ plt.savefig("MAPE_Train"+model_name+".jpeg", format='jpeg', dpi=1000)
 plt.show()
 
 
+# Data Akurasi dari gambar
+# accuracy_train = [68.65, 68.86, 69.00, 68.86, 67.84, 67.34, 68.95, 68.99, 68.86, 68.66, 
+#                   68.61, 68.95, 70.05, 69.03, 67.21, 68.69, 68.70, 68.98, 69.17, 68.36]
+accuracy_train = [77.25, 77.79, 77.19, 77.22, 78.14, 76.88, 77.01, 77.73, 76.19, 
+                  77.06, 77.14, 76.46, 78.11, 78.26, 77.65, 75.05, 76.33, 77.40, 
+                  76.86, 77.04]
+data_accuracy_train = [i+1 for i in range(len(accuracy_train))]
+
+# Plot Akurasi Training dengan garis tebal dan grid horizontal
+plt.figure(figsize=(14, 7))
+plt.plot(data_accuracy_train, accuracy_train, linewidth=2)  # Set garis lebih tebal
+plt.title('Akurasi Data Training')
+plt.ylabel('Akurasi (%)')
+plt.xlabel('Percobaan')
+plt.legend(['Akurasi'], loc='upper right')
+plt.grid(True)  # Tampilkan hanya grid horizontal
+plt.savefig("Akurasi_Train_"+model_name+".jpeg", format='jpeg', dpi=1000)
+plt.show()
